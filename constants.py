cdd364d14e229c9caee1d252a13500b9da21c21b
Sudo Paoo <104471005+sudo-paoo@users.noreply.github.com>
2022-05-27 12:42:35 +0800

    initial commit

---
+++ constants.py
@@ -0,0 +1,8 @@
+TG_TOKEN = 'YOUR TOKEN FROM @BotFather'
+DICT_URL = 'https://api.dictionaryapi.dev/api/v2/entries/en/'
+TIKTOK_URL = 'https://host.single-developers.software/tiktok?url='
+QR_URL = 'https://api.qrserver.com/v1/create-qr-code/?size=360x360&data='
+NASA_API = 'From https://api.nasa.gov/'
+NASA_URL = 'https://api.nasa.gov/planetary/apod?api_key='
+U_FACTS = 'https://uselessfacts.jsph.pl/random.json?language=en'
+MOVIE_URL_API = 'https://www.omdbapi.com/?apikey=<api from https://www.omdbapi.com/>&t='
